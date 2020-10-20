e9bc88bb3e605bef3362936e61af7ea5436185dd
Claisse Martinez <claisse.martinez@gmail.com>
2020-10-20 08:37:01 +0800

    Update 10202020

--- resources/views/content/gallery.blade.php
+++ resources/views/content/gallery.blade.php
@@ -12,97 +12,35 @@
             <h2>ILoveMyCountry.PH</h2>
           </div>
         </div>
-        <div>
             <div class="row mx-auto justify-content-center">
-                <div class="col-sm-10 col-xl-12 text-center">
+                <div class="col-sm-12 col-xl-12 text-center">
                   <div class="row mx-auto justify-content-center">
                     <div class="col-sm-10 col-xl-12 text-center">
                       <h2>RUN FOR FRAUD FREE PHILIPPINES 2019</h2>
                     </div>
                   </div>
-                    <div id="rfffp" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators" role="listbox">
-                            <li data-target="#rfffp" data-slide-to="0" class="active"></li>
-                            <li data-target="#rfffp" data-slide-to="1"></li>
-                            <li data-target="#rfffp" data-slide-to="2"></li>
-                            <li data-target="#rfffp" data-slide-to="3"></li>
-                            <li data-target="#rfffp" data-slide-to="4"></li>
-                            <li data-target="#rfffp" data-slide-to="5"></li>
-                            <li data-target="#rfffp" data-slide-to="6"></li>
-                            <li data-target="#rfffp" data-slide-to="7"></li>
-                            <li data-target="#rfffp" data-slide-to="8"></li>
-                            <li data-target="#rfffp" data-slide-to="9"></li>
-                            <li data-target="#rfffp" data-slide-to="10"></li>
-                            <li data-target="#rfffp" data-slide-to="11"></li>
-                            <li data-target="#rfffp" data-slide-to="12"></li>
-                            <li data-target="#rfffp" data-slide-to="13"></li>
-                            <li data-target="#rfffp" data-slide-to="14"></li>
-                            <li data-target="#rfffp" data-slide-to="15"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-1.jpg" alt="Slide 1">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-2.jpg" alt="Slide 2">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-3.jpg" alt="Slide 3">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-4.jpg" alt="Slide 4">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-5.jpg" alt="Slide 5">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-6.jpg" alt="Slide 6">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-7.jpg" alt="Slide 7">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-8.jpg" alt="Slide 8">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-9.jpg" alt="Slide 9">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-10.jpg" alt="Slide 10">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-11.jpg" alt="Slide 11">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-12.jpg" alt="Slide 12">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-13.jpg" alt="Slide 13">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-14.jpg" alt="Slide 14">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-15.jpg" alt="Slide 15">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img img-carousel" src="image/rfffp2019/rfffp2019img-16.jpg" alt="Slide 16">
-                            </div>
+                  <div class="row mx-auto justify-content-center">
+                    <div class="col-sm-10 col-xl-12">
+                        <div class="rfffp2019">
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-1"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-2"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-3"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-4"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-5"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-6"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-7"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-8"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-9"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-10"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-11"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-12"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-13"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-14"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-15"></div>
+                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-16"></div>
                         </div>
-                        <a class="carousel-control-prev" href="#rfffp" role="button"
-                            data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#rfffp" role="button"
-                            data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
                 </div>
             </div>
-        </div>
         <div class="row">
           <div class="divider"></div>
       </div>
@@ -112,66 +50,7 @@
             <h2>TAAL VOLCANO RELIEF OPERATION</h2>
           </div>
         </div>
-        <div class="row mx-auto justify-content-center">
-            <div class="col-sm-10 col-xl-12 text-center">
-                <div id="tvor" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators" role="listbox">
-                      <li data-target="#tvor" data-slide-to="0" class="active"></li>
-                      <li data-target="#tvor" data-slide-to="1"></li>
-                      <li data-target="#tvor" data-slide-to="2"></li>
-                      <li data-target="#tvor" data-slide-to="3"></li>
-                      <li data-target="#tvor" data-slide-to="4"></li>
-                      <li data-target="#tvor" data-slide-to="5"></li>
-                      <li data-target="#tvor" data-slide-to="6"></li>
-                      <li data-target="#tvor" data-slide-to="7"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/1.jpg"
-                            alt="Slide 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/2.jpg"
-                            alt="Slide 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/3.jpg"
-                            alt="Slide 3">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/4.jpg"
-                            alt="Slide 4">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/5.jpg"
-                            alt="Slide 5">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/6.jpg"
-                            alt="Slide 6">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/7.jpg"
-                            alt="Slide 7">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/taal-volcano-relief-operation/8.jpg"
-                            alt="Slide 8">
-                    </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#tvor" role="button"
-                        data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#tvor" role="button"
-                        data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            </div>
-        </div>
+       
         <div class="row">
           <div class="divider"></div>
       </div>
@@ -181,74 +60,7 @@
             <h2>HEROIC RUN 2018</h2>
           </div>
         </div>
-        <div class="row mx-auto justify-content-center">
-            <div class="col-sm-10 col-xl-12 text-center">
-                <div id="hr18" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators" role="listbox">
-                      <li data-target="#hr18" data-slide-to="0" class="active"></li>
-                      <li data-target="#hr18" data-slide-to="1"></li>
-                      <li data-target="#hr18" data-slide-to="2"></li>
-                      <li data-target="#hr18" data-slide-to="3"></li>
-                      <li data-target="#hr18" data-slide-to="4"></li>
-                      <li data-target="#hr18" data-slide-to="5"></li>
-                      <li data-target="#hr18" data-slide-to="6"></li>
-                      <li data-target="#hr18" data-slide-to="7"></li>
-                      <li data-target="#hr18" data-slide-to="8"></li>
-                      <li data-target="#hr18" data-slide-to="9"></li>
-                      <li data-target="#hr18" data-slide-to="10"></li>
-                      <li data-target="#hr18" data-slide-to="11"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img class="img img-carousel" src="image/heroic-run2018/1.png" alt="Slide 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/2.png" alt="Slide 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/3.png" alt="Slide 3">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/4.png" alt="Slide 4">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/5.png" alt="Slide 5">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/6.png" alt="Slide 6">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/7.png" alt="Slide 7">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/8.png" alt="Slide 8">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/9.png" alt="Slide 9">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="image/heroic-run2018/10.png" alt="Slide 10">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/11.png" alt="Slide 11">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-carousel" src="image/heroic-run2018/12.png" alt="Slide 12">
-                    </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#hr18" role="button"
-                        data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#hr18" role="button"
-                        data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            </div>
-        </div>
+        
     </div>
     </div>
     <div class="row">
@@ -256,3 +68,9 @@
   </div>
 </div>
 </div>
+
+<!--end-home-->
+
+
+
+
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,17 +6,15 @@
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('css/main-style.css')}}">
     
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
-
- 
+    
+    
     <title>ILOVEMYCOUNTRY.PH</title>
 </head>
 
 <body>
     @include('layout/navbar')
    
-    @yield('style')
+    @yield('styles')
     @yield('content')
     @yield('script')
 
