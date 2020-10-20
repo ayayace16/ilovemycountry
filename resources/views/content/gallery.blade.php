530324902fd40c27c54e9f83ff9a4bef8b159877
Claisse Martinez <claisse.martinez@gmail.com>
2020-10-20 09:22:55 +0800

    Update 10202020

--- resources/views/content/gallery.blade.php
+++ resources/views/content/gallery.blade.php
@@ -19,28 +19,6 @@
                         <h2>RUN FOR FRAUD FREE PHILIPPINES 2019</h2>
                     </div>
                 </div>
-                <div class="row mx-auto justify-content-center">
-                    <div class="col-sm-10 col-xl-12">
-                        <div class="rfffp2019">
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-1"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-2"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-3"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-4"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-5"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-6"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-7"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-8"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-9"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-10"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-11"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-12"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-13"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-14"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-15"></div>
-                            <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-16"></div>
-                        </div>
-                    </div>
-                </div>
                 <div class="row">
                     <div class="divider"></div>
                 </div>
--- resources/views/content/programs.blade.php
+++ resources/views/content/programs.blade.php
@@ -7,125 +7,129 @@
          <div class="row">
              <div class="divider"></div>
          </div>
+         <div class="row mx=auto justify-content-center">
+             <div class="col-sm-10 col-md-4 col-xl-4">
+                 <img src="image/Programs/programs-1.jpg" width="289.78px" height="196.06px">
+             </div>
+             <div class="col-xl-6">
+                 <a href="#">ILOVEMYCOUNTRY FOR THE FAMILY</a>
+                 <p>Talks and seminars on how we love our country more and value formation for
+                     the
+                     family.
+                     <ul>
+                         <li>Feeding Program</li>
+                         <li>Calamity Assistance</li>
+                     </ul>
+                 </p>
+             </div>
+         </div>
+         <hr>
+         <div class="row mx=auto justify-content-center">
+             <div class="col-xl-6">
+                 <a href="#">ILOVEMYCOUNTRY IN COMMUNITY</a>
+                 <p>Talks and seminars on how to love our country more, livelihood training,
+                     and values formation for
+                     the community.
+                     This program also includes:
+                     <ul>
+                         <li>iLoveMyCountry Feeing Program</li>
+                         <li>Calamity Assistance Program</li>
+                         <li>Livelihood and Financial Literacy Seminar</li>
+                     </ul>
+                 </p>
+             </div>
+             <div class="col-sm-10 col-md-4 col-xl-4">
+                 <img src="image/Programs/programs-2.png" width="289.78px" height="196.06px">
+             </div>
+         </div>
+         <hr>
+         <div class="row mx=auto justify-content-center">
+             <div class="col-sm-10 col-md-4 col-xl-4">
+                 <img src="image/Programs/programs-3.png" width="289.78px" height="196.06px">
+             </div>
+             <div class="col-xl-6">
+                 <div class="programc">
+                     <a href="#">ILOVEMYCOUNTRY IN SCHOOLS</a>
+                     <p>Talks and symposia on how to love our country more to make it a
+                         Fraud-Free country.</p>
+                 </div>
+             </div>
+         </div>
+         <hr>
+         <div class="row mx=auto justify-content-center">
+             <div class="col-xl-6">
+                 <a href="#">ILOVEMYCOUNTRY IN BUSINESS AND<br>PROFESSION</a>
+                 <p>Seminars, symposia, and fora in a corporate and professional setting on how
+                     a
+                     company can
+                     contribute to make the Philippines a Fraued-Free country. This program is in
+                     coordination
+                     with
+                     Professionals of the Future (POF)</p>
+             </div>
+             <div class="col-sm-10 col-md-4 col-xl-4">
+                 <img src="image/Programs/programs-4.jpg" width="289.78px" height="196.06px">
+             </div>
+         </div>
+         <div class="row">
+             <div class="divider"></div>
+         </div>
+     </div>
+ </div>
+ <div id="our-program">
+     <div class="parallax">
+         <h1>MEDIA CAMPAIGNS & AWARDS</h1>
+     </div>
+     <div class="container">
+         <div class="row">
+             <div class="divider"></div>
+         </div>
+         <div class="row mx-auto justify-content-center">
+             <div class="col-xl-12">
                  <div class="row mx=auto justify-content-center">
                      <div class="col-sm-10 col-md-4 col-xl-4">
-                         <img src="image/Programs/programs-1.jpg" width="289.78px" height="196.06px">
+                         <img src="image/Programs/mca/mca-1.png" width="289.78px" height="196.06px">
                      </div>
                      <div class="col-xl-6">
-                         <a href="#">ILOVEMYCOUNTRY FOR THE FAMILY</a>
-                         <p>Talks and seminars on how we love our country more and value formation for
-                             the
-                             family.
-                             <ul>
-                                 <li>Feeding Program</li>
-                                 <li>Calamity Assistance</li>
-                             </ul>
-                         </p>
-                     </div>
-                 </div>
-                 <hr>
-                 <div class="row mx=auto justify-content-center">
-                     <div class="col-xl-6">
-                         <a href="#">ILOVEMYCOUNTRY IN COMMUNITY</a>
-                         <p>Talks and seminars on how to love our country more, livelihood training,
-                             and values formation for
-                             the community.
-                             This program also includes:
-                             <ul>
-                                 <li>iLoveMyCountry Feeing Program</li>
-                                 <li>Calamity Assistance Program</li>
-                                 <li>Livelihood and Financial Literacy Seminar</li>
-                             </ul>
-                         </p>
-                     </div>
-                     <div class="col-sm-10 col-md-4 col-xl-4">
-                         <img src="image/Programs/programs-2.png" width="289.78px" height="196.06px">
-                     </div>
-                 </div>
-                 <hr>
-                 <div class="row mx=auto justify-content-center">
-                     <div class="col-sm-10 col-md-4 col-xl-4">
-                         <img src="image/Programs/programs-3.png" width="289.78px" height="196.06px">
-                     </div>
-                     <div class="col-xl-6">
-                         <div class="programc">
-                             <a href="#">ILOVEMYCOUNTRY IN SCHOOLS</a>
-                             <p>Talks and symposia on how to love our country more to make it a
-                                 Fraud-Free country.</p>
-                         </div>
-                     </div>
-                 </div>
-                 <hr>
-                 <div class="row mx=auto justify-content-center">
-                     <div class="col-xl-6">
-                         <a href="#">ILOVEMYCOUNTRY IN BUSINESS AND<br>PROFESSION</a>
-                         <p>Seminars, symposia, and fora in a corporate and professional setting on how
+                         <a href="#">ILOVEMYCOUNTRY RUN</a>
+                         <p class="text-left">A year fun-run gathering of Extraordinary and Heroic Filipino.
+                             Participants
+                             of this event run for
                              a
-                             company can
-                             contribute to make the Philippines a Fraued-Free country. This program is in
-                             coordination
-                             with
-                             Professionals of the Future (POF)</p>
+                             ause
+                             and advocates of ILMCP vision and mission.</p>
                      </div>
+                 </div>
+                 <hr>
+                 <div class="row mx=auto justify-content-center">
+                     <div class="col-xl-6">
+                         <a href="#">EXTRAORDINARY AND HEROIC<br>FILIPINO AWARDS</a>
+                         <p class="text-left">The highest award given to Filipino who explicit Extraordinary and
+                             Heroic deeds in
+                             showing
+                             the
+                             love for the country.</p>
+                     </div>
+                     <div class="col-sm-10 col-md-4 col-xl-4 text-center">
+                         <img src="image/Programs/mca/mca-2.png" width="289.78px" height="196.06px">
+                     </div>
+                 </div>
+                 <hr>
+                 <div class="row mx=auto justify-content-center">
                      <div class="col-sm-10 col-md-4 col-xl-4">
-                         <img src="image/Programs/programs-4.jpg" width="289.78px" height="196.06px">
+                         <img src="image/Programs/mca/mca-3.png" width="289.78px" height="196.06px">
+                     </div>
+                     <div class="col-xl-6">
+                         <a href="#">SOCIAL MEDIA CAMPAIGNS</a>
+                         <p class="text-left">An everyday posting of photos, videos, blogs an articles showing
+                             how to
+                             love your country
+                             and
+                             uplifting the Philippines as a nation.</p>
                      </div>
                  </div>
-                 <div class="row">
-                     <div class="divider"></div>
-                 </div>
-                 <div class="parallax">
-                     <h1>MEDIA CAMPAIGNS & AWARDS</h1>
-                 </div>
-                 <div class="row">
-                     <div class="divider"></div>
-                 </div>
-                 <div class="row mx-auto justify-content-center">
-                     <div class="col-xl-12">
-                         <div class="row mx=auto justify-content-center">
-                             <div class="col-sm-10 col-md-4 col-xl-4">
-                                 <img src="image/Programs/mca/mca-1.png" width="289.78px" height="196.06px">
-                             </div>
-                             <div class="col-xl-6">
-                                 <a href="#">ILOVEMYCOUNTRY RUN</a>
-                                 <p class="text-left">A year fun-run gathering of Extraordinary and Heroic Filipino.
-                                     Participants
-                                     of this event run for
-                                     a
-                                     ause
-                                     and advocates of ILMCP vision and mission.</p>
-                             </div>
-                         </div>
-                         <hr>
-                         <div class="row mx=auto justify-content-center">
-                             <div class="col-xl-6">
-                                 <a href="#">EXTRAORDINARY AND HEROIC<br>FILIPINO AWARDS</a>
-                                 <p class="text-left">The highest award given to Filipino who explicit Extraordinary and
-                                     Heroic deeds in
-                                     showing
-                                     the
-                                     love for the country.</p>
-                             </div>
-                             <div class="col-sm-10 col-md-4 col-xl-4 text-center">
-                                 <img src="image/Programs/mca/mca-2.png" width="289.78px" height="196.06px">
-                             </div>
-                         </div>
-                         <hr>
-                         <div class="row mx=auto justify-content-center">
-                             <div class="col-sm-10 col-md-4 col-xl-4">
-                                 <img src="image/Programs/mca/mca-3.png" width="289.78px" height="196.06px">
-                             </div>
-                             <div class="col-xl-6">
-                                 <a href="#">SOCIAL MEDIA CAMPAIGNS</a>
-                                 <p class="text-left">An everyday posting of photos, videos, blogs an articles showing
-                                     how to
-                                     love your country
-                                     and
-                                     uplifting the Philippines as a nation.</p>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
+             </div>
+         </div>
          <div class="row">
              <div class="divider"></div>
          </div>
