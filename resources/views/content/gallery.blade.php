994f052416853246d498aa2369a01a6f03753294
Claisse Martinez <claisse.martinez@gmail.com>
2020-10-20 08:57:00 +0800

    Update gallery.blade.php

--- resources/views/content/gallery.blade.php
+++ resources/views/content/gallery.blade.php
@@ -8,18 +8,18 @@
             <div class="divider"></div>
         </div>
         <div class="row mx-auto justify-content-center">
-          <div class="col-sm-10 col-xl-12 text-center">
-            <h2>ILoveMyCountry.PH</h2>
-          </div>
+            <div class="col-sm-10 col-xl-12 text-center">
+                <h2>ILoveMyCountry.PH</h2>
+            </div>
         </div>
-            <div class="row mx-auto justify-content-center">
-                <div class="col-sm-12 col-xl-12 text-center">
-                  <div class="row mx-auto justify-content-center">
+        <div class="row mx-auto justify-content-center">
+            <div class="col-sm-12 col-xl-12 text-center">
+                <div class="row mx-auto justify-content-center">
                     <div class="col-sm-10 col-xl-12 text-center">
-                      <h2>RUN FOR FRAUD FREE PHILIPPINES 2019</h2>
+                        <h2>RUN FOR FRAUD FREE PHILIPPINES 2019</h2>
                     </div>
-                  </div>
-                  <div class="row mx-auto justify-content-center">
+                </div>
+                <div class="row mx-auto justify-content-center">
                     <div class="col-sm-10 col-xl-12">
                         <div class="rfffp2019">
                             <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-1"></div>
@@ -39,38 +39,36 @@
                             <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-15"></div>
                             <div><img src="image/rfffp2019/rfffp2019img-1.jpg" alt="Image-16"></div>
                         </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="divider"></div>
+                </div>
+                <div>
+                    <div class="row mx-auto justify-content-center">
+                        <div class="col-sm-10 col-xl-12 text-center">
+                            <h2>TAAL VOLCANO RELIEF OPERATION</h2>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="divider"></div>
+                    </div>
+                    <div>
+                        <div class="row mx-auto justify-content-center">
+                            <div class="col-sm-10 col-xl-12 text-center">
+                                <h2>HEROIC RUN 2018</h2>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="divider"></div>
                 </div>
             </div>
-        <div class="row">
-          <div class="divider"></div>
-      </div>
-      <div>
-        <div class="row mx-auto justify-content-center">
-          <div class="col-sm-10 col-xl-12 text-center">
-            <h2>TAAL VOLCANO RELIEF OPERATION</h2>
-          </div>
         </div>
-       
-        <div class="row">
-          <div class="divider"></div>
-      </div>
-      <div>
-        <div class="row mx-auto justify-content-center">
-          <div class="col-sm-10 col-xl-12 text-center">
-            <h2>HEROIC RUN 2018</h2>
-          </div>
-        </div>
-        
     </div>
-    </div>
-    <div class="row">
-      <div class="divider"></div>
-  </div>
-</div>
 </div>
 
 <!--end-home-->
-
-
-
-
