702196101f16fd5b91e78beff46d13bdf66a84a7
Claisse Martinez <claisse.martinez@gmail.com>
2020-10-27 17:01:48 +0800

    Upate 10272020

--- resources/views/admin-layout/admin-sidenav.blade.php
+++ resources/views/admin-layout/admin-sidenav.blade.php
@@ -1,4 +1,5 @@
-<div class="sidenav" hidden>
+<div class="sidenav">
+    <img src="../image/cropped-logo.jpg" alt="ilovemycountry" width="100" height="75">
     <a href="#home">Home</a>
     <a href="#avocacy-team">Advocay Team</a>
     <a href="#gallery">Gallery</a>
@@ -6,21 +7,7 @@
     <a href="#souvenir">Souvenir</a>
     <a href="#partners-sposnors">Partners and Sponsors</a>
     <a href="#contact">Contact</a>
-
-    <span onclick="openNav()"></span>
 </div>
 
-<div id="sidenav" class="sidenav">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="#">About</a>
-    <a href="#">Services</a>
-    <a href="#">Clients</a>
-    <a href="#">Contact</a>
-  </div>
-  
-  <div id="main">
-    <h2>Sidenav Push Example</h2>
-    <p>Click on the element below to open the side navigation menu, and push this content to the right.</p>
-    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
-  </div>
+
   
