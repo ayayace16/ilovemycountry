cc1373660dc31b58ce35f8236817e7e7d7554877
Claisse Martinez <claisse.martinez@gmail.com>
2020-10-28 08:15:41 +0800

    Update

--- resources/views/admin-layout/admin-sidenav.blade.php
+++ resources/views/admin-layout/admin-sidenav.blade.php
@@ -1,13 +1,31 @@
-<div class="sidenav">
-    <img src="../image/cropped-logo.jpg" alt="ilovemycountry" width="100" height="75">
-    <a href="#home">Home</a>
-    <a href="#avocacy-team">Advocay Team</a>
-    <a href="#gallery">Gallery</a>
-    <a href="#news-events">News and Events</a>
-    <a href="#souvenir">Souvenir</a>
-    <a href="#partners-sposnors">Partners and Sponsors</a>
-    <a href="#contact">Contact</a>
-</div>
+<nav class="sidenav">
+    <div class="sidenav-logo">
+    <img src="../image/cropped-logo.png" alt="ilovemycountry" width="100" height="75">
+    </div>
+    <ul>
+        <li class="active">
+            <a href="#home">Home</a>
+        </li>
+        <li>
+            <a href="#avocacy-team">Advocay Team</a>
+        </li>
+        <li>
+            <a href="#gallery">Gallery</a>
+        </li>
+        <li>
+            <a href="#news-events">News and Events</a>
+        </li>
+        <li>
+            <a href="#souvenir">Souvenir</a>
+        </li>
+        <li>
+            <a href="#partners-sposnors">Partners and Sponsors</a>
+        </li>
+        <li>
+            <a href="#contact">Contact</a>
+        </li>
+    </ul>   
+</nav>
 
 
   
